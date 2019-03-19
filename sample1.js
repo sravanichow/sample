b8ce22928b93778fb86bbabda459df97bec2b3ae
sravani A <sravanichowdary897@gmail.com>
2019-03-19 14:54:24 +0530

    v-portav sample

---
+++ sample1.js
@@ -0,0 +1,13 @@
+let msg="hai"
+
+function myfun(){
+ let msg = 'sam hai'
+    console.log("message",msg);
+    
+}
+console.log("msg before",msg);
+console.log( "fun1",myfun);
+
+console.log("msg after",msg);
+// console.log( "fun2",myfun());
+// console.log("msg after calling 2nd time",msg);
---
+++ sample2.js
@@ -0,0 +1,28 @@
+
+(function sayhai(){
+    console.log("first function executed");
+    
+})()
+// console.log("log meg",())
+
+// sayhai()
+// let sample = function say(){
+//     console.log("hai varun !!!!!");
+    
+// }
+// let sam=sample
+// console.log("sam",sam());
+
+// console.log("calling vth fun name",sayhai());
+// console.log("calling vth function variable name",sample());
+
+let ans=(a,b)=>{
+ console.log("welcome to name "+" city name ");
+//  return a+b;
+}
+ans(3,2)
+
+function add(a,b){
+  return a+b;
+}
+add(2,3)
